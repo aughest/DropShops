21754adf72b1269a8d97f7f484b80f1ebf375534
Aughest <aughestptr@gmail.com>
2022-01-14 22:45:48 +0700

    delete link

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,8 +35,6 @@ return [
             'root' => storage_path('app'),
         ],
 
-        
-
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
